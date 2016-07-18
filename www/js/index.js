ebcc9827a3c6106e40fc5fbe3fd4dace3158c58e
Evgeniy Bannikov <evgeny@bannikov.info>
2016-07-18 06:34:47 +0500

    switch to hw

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,13 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-      console.log('Received Event: ' + id);
-      angular.bootstrap(document, ["App"]);
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
     }
 };
